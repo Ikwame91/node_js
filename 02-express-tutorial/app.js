adee95c93089dc3a38da5f9e929eab3b6cde446b
ikwame <iaagyapong@st.knust.edu.gh>
2024-11-07 22:43:26 +0000

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,12 @@
-console.log('Express Tutorial')
+const express = require('express')
+
+const app = express();
+
+
+app.get('/', (req,res)=>{
+    res.json()
+
+})
+app.listen(5000, ()=>{
+    console.log(" Server is listening on port 5000.....")
+})
