11ae89a992301ee23d3a352c254cf9d243e80a1d
ikwame <iaagyapong@st.knust.edu.gh>
2024-11-08 22:55:21 +0000

    node learn ..101

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,30 +1,28 @@
 const express = require("express");
-const { products } = require("./data");
-
+const logger = require('./practice/logger')
 const app = express();
+const authorize = require('./practice/authorize')
+app.use([logger,authorize])
+
+// req => middleware => res
+
+
 
 app.get("/", (req, res) => {
-  res.send('<h1> Home Page </h1><a href ="/api/products"> products </a>');
+  res.send(" Home");
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if(!singleProduct){
-    return res.status(404).send('product does not exist')
-  }
-  res.json(singleProduct);
+app.get("/about", (req, res) => {
+  res.send(" About page");
 });
 
+app.get("/products", (req, res) => {
+  res.send(" products page");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send(" items page");
+});
 app.listen(5000, () => {
   console.log(" Server is listening on port 5000.....");
 });
--- 02-express-tutorial/practice/param-query.js
+++ 02-express-tutorial/practice/param-query.js
@@ -19,12 +19,36 @@ app.get("/api/products/:productID", (req, res) => {
   const singleProduct = products.find(
     (product) => product.id === Number(productID)
   );
-  if(!singleProduct){
-    return res.status(404).send('product does not exist')
+  if (!singleProduct) {
+    return res.status(404).send("product does not exist");
   }
   res.json(singleProduct);
 });
 
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if(sortedProducts.length<1){
+    // res.status(200).send('no products match your search')
+    return res.status(200).json({sucess: true, data :[]})
+  }
+   res.status(200).json(sortedProducts);
+});
 app.listen(5000, () => {
   console.log(" Server is listening on port 5000.....");
 });
